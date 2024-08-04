896340c0d188c9bb0d675c96b1bebd1b6c3425ca
peculiaruc <peculiarumeh02@gmail.com>
2024-08-04 14:55:27 +0100

    basics

---
+++ basics.py
@@ -0,0 +1,32 @@
+# Basics: fundamental syntax, data types,variables and operations
+
+# Fundamental rules= 
+# 1. Operators:
+#  a. Exponent **, eg    
+  2**8 == 256 # type: ignore
+#  b. Modulus/remainder %, eg 
+  23 % 7 == 2 # type: ignore
+#  c. Integer division/floored quotient //, eg 
+  23 // 7 == 3 # type: ignore
+#  d. Division /, eg
+  12/ 4 == 3 # type: ignore
+#  e. Multiplication *, eg 
+  2 * 4 == 8 # type: ignore
+#  f. Subtraction -, eg 
+  18 - 9 == 9 # type: ignore
+#  g. Addition +, eg 
+  30 + 5 == 35 # type: ignore
+
+# order of operation -> ** follow by /, //, % and follow by + and -
+
+# 2. Data types:
+#  a. Integer (numbers) eg age = 30 
+#  b. float (decimal numbers) eg pi = 3,456
+#  c. String (squencses of characters enclosed in quotes) eg "My name is Pec"
+#  d. Booleans (logical values, True or False) eg jake is running = True or false
+
+# Control Flow: conditional statements(if-else) and loops(for, while).
+# Make deciions and reapt tasks based on conditions.
+# Functions: define functions to organise codes, improve reusability etc.
+# Data structures: Lists, tuples, dictionaries, sets(store and organize data)
+# Object-oriented Programming(OOP): Classes, objects, inheritance, polymorphism(to structure codes in a modular and reusable way)
